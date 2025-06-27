094bb00cb4dd7d16da966189b9fe64d8b6e59e95
Moxipod <guy.shachar19409@gmail.com>
2025-06-27 18:16:17 +0300

    fixed two_complement

--- two_complement.py
+++ two_complement.py
@@ -7,29 +7,17 @@ def decimal_to_binary(num, bitsize):
     binary_num = bin(num)[2:]  # remove '0b'
     return binary_num.zfill(bitsize)  # pad with zeros
 
-def two_complement(binary_num):
+def two_complement(binary_num,bitsize):
     # Step 1: Flip bits
-    inverted = ''.join('1' if bit == '0' else '0' for bit in binary_num)
-    
-    # Step 2: Add 1 manually
-    result = list(inverted)
-    carry = 1
-    for i in range(len(result)-1, -1, -1):
-        if result[i] == '1' and carry == 1:
-            result[i] = '0'
-        elif result[i] == '0' and carry == 1:
-            result[i] = '1'
-            carry = 0
-            break
-    if carry == 1:
-        result.insert(0, '1')  # overflow for full addition
-    return ''.join(result[-len(binary_num):])  # keep same bit length
-
+    #inverted = ''.join('1' if bit == '0' else '0' for bit in binary_num)
+    flipped = ''.join('1' if bit == '0' else '0' for bit in binary_num)
+    complement_int = int(flipped, 2) + 1
+    return decimal_to_binary(complement_int,bitsize)
 def main():
     positive_decimal, bit_size = get_input()
 
     binary_representation = decimal_to_binary(positive_decimal, bit_size)
-    negative_binary = two_complement(binary_representation)
+    negative_binary = two_complement(binary_representation,bit_size)
 
     print(f"Input decimal number: {positive_decimal}")
     print(f"Binary representation: {binary_representation}")
