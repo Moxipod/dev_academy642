9d1995cb20f9a65de1197e24a318a59b4e8c537b
Moxipod <guy.shachar19409@gmail.com>
2025-06-27 19:37:45 +0300

    finished the bonus

---
+++ file_encryption_bonus1.py
@@ -0,0 +1,13 @@
+#import two_complement
+filename = input("Enter filename: with end . for example if name is text enter text.txt you need to have that file in same folder") #try with dogi.jpeg
+xor_key = 13
+with open(filename, 'r+b') as f:
+    data =f.read()
+    f.seek(0) #reset file pointer to zero
+    for byte in data:
+        f.write(bytes([byte ^ xor_key]))
+
+        
+    
+
+      
